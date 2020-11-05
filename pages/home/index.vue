ba7633217c19dfe693bd870362699c1e81de5c35
k <kunkunzzzz@outlook.com>
2020-11-05 12:04:57 +0800

    feat: 列表分页-页码处理

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -63,6 +63,25 @@
               <span>Read more...</span>
             </nuxt-link>
           </div>
+
+          <!-- 分页列表 -->
+          <nav>
+            <ul class="pagination">
+              <li
+                class="page-item"
+                :class="{ active: item === page }"
+                v-for="item in totalPage"
+                :key="item"
+              >
+                <nuxt-link
+                  class="page-link"
+                  :to="{ name: 'home', query: { page: item } }"
+                  >{{ item }}</nuxt-link
+                >
+              </li>
+            </ul>
+          </nav>
+          <!-- /分页列表 -->
         </div>
 
         <div class="col-md-3">
@@ -91,17 +110,25 @@ import { getArticles } from '@/api/article'
 
 export default {
   name: 'HomeIndex',
-  async asyncData() {
-    const page = 1
-    const limit = 10
+  async asyncData({ query }) {
+    const page = Number.parseInt(query.page || 1)
+    const limit = 20
     const { data } = await getArticles({
       limit,
-      offset: (page - 1) * limit
+      offset: (page - 1) * limit,
     })
     return {
       articles: data.articles,
       articlesCount: data.articlesCount,
+      page,
+      limit,
     }
+  },
+  watchQuery: ['page'], // 监听 page 查询参数的变化
+  computed: {
+    totalPage() {
+      return Math.ceil(this.articlesCount / this.limit)
+    },
   },
 }
 </script>
